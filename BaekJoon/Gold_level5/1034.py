3de4a0a13c1a2393ef84e3847f8c378edf2c1341
dataenggu <dsolkim1030@gmail.com>
2020-12-10 22:47:13 +0900

    Code improved (shorter)

--- BaekJoon/Gold_level5/1034.py
+++ BaekJoon/Gold_level5/1034.py
@@ -1,28 +1,17 @@
 import sys
 
-
-def search(arr, row):
-    cnt = 0
-    for i in range(row + 1, len(arr)):
-        flag = True
-        for j in range(len(arr[0])):
-            if arr[i][j] != arr[row][j]:
-                flag = False
-                break
-        if flag: cnt += 1
-    return cnt
-
-
 N, M = map(int, sys.stdin.readline().rstrip().split(" "))
-arr = [[int(ele) for ele in sys.stdin.readline().rstrip()] for i in range(N)]
-K = int(sys.stdin.readline())
+dic = {}
+for i in range(N):
+    s = sys.stdin.readline().rstrip()
+    if s in dic:
+        dic[s] = dic.get(s) + 1
+    else:
+        dic[s] = 1
+K = int(sys.stdin.readline().rstrip())
 res = 0
-for row in range(N):
-    cnt = 0
-    rowCnt = 1
-    for col in range(M):
-        if arr[row][col] == 0: cnt += 1
-    if cnt <= K and (K - cnt) % 2 == 0:
-        rowCnt += search(arr, row)
-        res = max(res, rowCnt)
+for ele in dic:
+    z = ele.count("0")
+    if K >= z and (K - z) % 2 == 0:
+        res = max(res, dic.get(ele))
 print(res)
