6aced60b698cc44be813d14bf8faaa1d3e6f667e
dataenggu <dsolkim1030@gmail.com>
2020-12-13 21:20:35 +0900

    Code improvement

--- BaekJoon/Gold_level5/1092.py
+++ BaekJoon/Gold_level5/1092.py
@@ -1,24 +1,23 @@
 import sys
 
+#  크레인과 boxes 를 정렬한 후, 박스를 옮길 수 있는 크레인 중 가장 작업량이 한가한 크레인에 box를 할당한다.
+#  그리고 작업 스케쥴에 있는 크레인 중 가장 횟수가 많은 것을 반환
 N = int(sys.stdin.readline())
 cranes = sorted(list(map(int, sys.stdin.readline().split(" "))), reverse=True)
 boxNum = int(sys.stdin.readline())
 boxes = sorted(list(map(int, sys.stdin.readline().split(" "))), reverse=True)
-res = cnt = 0
 if cranes[0] < boxes[0]:
     print(-1)
     exit()
-visit = [0 for i in range(boxNum)]
-while cnt < boxNum:
-    for crane in cranes:
-        flag = False
-        for i in range(len(boxes)):
-            if visit[i] == 0 and crane >= boxes[i]:
-                visit[i] = 1
-                cnt += 1
-                flag = True
-                break
-        if not flag:
-            cranes.remove(crane)
-    res += 1
-print(res)
+# 크레인 작업 스케쥴
+craneSchedule = [0 for i in range(N)]
+for box in boxes:
+    idx, capa = 0, craneSchedule[0]
+    for i in range(len(cranes)):
+        if box <= cranes[i] and craneSchedule[i] <= capa:
+            idx = i
+            capa = craneSchedule[i]
+        elif box > cranes[i]:
+            break
+    craneSchedule[idx] = capa + 1
+print(max(craneSchedule))
